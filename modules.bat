962a8a25d9e895487102faf5abbbe3398956f40a
diniz <98988939+diniz-LG@users.noreply.github.com>
2023-01-07 12:21:17 -0300

    Add files via upload

---
+++ modules.bat
@@ -0,0 +1,14 @@
+pip install requests
+pip install Pillow
+pip install datetime
+pip install psutil
+pip install cryptography
+pip install discord
+pip install httpx
+pip install pypiwin32
+pip install colorap
+pip install wmi
+pip install alive-progress
+pip install colorama
+pip install wniutil
+pip install pycryptodome
